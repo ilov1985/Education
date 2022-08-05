aa7d170fc9e4d7d5af8c80ad7baf83afac807d5f
IlyaO <iloa1985@rambler.ru>
2022-08-05 16:23:34 +0300

    First save

---
+++ Sem1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите целое число a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите целое число b = ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+    Console.WriteLine("Максимальное число max = " + a);
+} 
+else 
+{
+    Console.WriteLine("Максимальное число max = " + b);
+}
