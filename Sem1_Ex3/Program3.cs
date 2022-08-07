7a145d754829612cd8a22bf7821eb98d72bf4e57
IlyaO <iloa1985@rambler.ru>
2022-08-07 13:51:20 +0300

    First save3

---
+++ Sem1_Ex3/Program3.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите целое число  ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 2 == 0)
+{
+    Console.WriteLine("Да, число " + a + " делится без остатка на 2");
+} 
+else
+{
+    Console.WriteLine("Нет, число " + a + " не делится без остатка на 2");
+} 
