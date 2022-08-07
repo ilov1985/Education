b2e544d41222d0b21b667f8346ec319114ad0cc8
IlyaO <iloa1985@rambler.ru>
2022-08-07 14:10:16 +0300

    First save4

---
+++ Sem1_Ex4/Program4.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите целое положительное число  ");
+int a = Convert.ToInt32(Console.ReadLine());
+int index = 1;
+Console.Write("Четные числа ");
+while (index < a )
+{
+    if (index%2 == 0) Console.Write(index + " ");
+    index++;
+}
+if (a%2 == 0) Console.Write(a);
