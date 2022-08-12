b89519ac193efb84835bc79879dcfc5dd5846066
ilov1985 <109591577+ilov1985@users.noreply.github.com>
2022-08-12 11:43:03 +0300

    Delete Sem2_Ex3 directory

--- Sem2_Ex3/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿Console.WriteLine("Выберите день");
-Console.WriteLine(" 1 - Понедельник \n 2 - Вторник \n 3 - Среда \n 4 - Четверг \n 5 - Пятница \n 6 - Суббота \n 7 - Воскресенье");
-int N = Convert.ToInt32(Console.ReadLine());
-if (N < 6) Console.WriteLine("нет, не выходной");
-else Console.WriteLine("да, выходной");
