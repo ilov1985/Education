f7bb640bd9396fb90b35884a261a3b6273decdc9
ilov1985 <109591577+ilov1985@users.noreply.github.com>
2022-08-12 11:43:36 +0300

    Delete Sem2_Ex1 directory

--- Sem2_Ex1/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿Console.Write("Введите трехзначное целое число ");
-int N = Convert.ToInt32(Console.ReadLine());
-Console.Write("Вторая цифра числа " + N + " это - " + (N%100)/10);
-
-// Строковый вариант:
-// Console.Write("Введите трехзначное целое число ");
-// string? N = Console.ReadLine();
-// Console.Write("Вторая цифра числа " + N + " это - " + N[1]);
