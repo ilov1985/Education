4b24f8cd5f73dec7ed81e8aa1d0866a6861384a7
ilov1985 <109591577+ilov1985@users.noreply.github.com>
2022-08-16 23:12:19 +0300

    Ex2

---
+++ Sem4_Ex2.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая
+// принимает на вход число и
+// выдает сумму цифр в числе.
+using System;
+
+
+static class Program
+{
+	static void Main()
+	{
+		Console.Write("Введите число: ");
+	    string str = Console.ReadLine();
+		int max =  str.Length;
+		int sum = 0;
+		int str1 = Convert.ToInt32(str);
+		for (int i = 0; i < max; i++)
+		{
+			int tmp = str1 % 10;
+			sum = sum + tmp;
+			str1 = str1 / 10;
+		}
+		Console.Write("Сумма цифр числа: " + sum);
+	}
+}
