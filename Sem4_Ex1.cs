f25ca4f045f3fe6e7a5af920f4e2862f08055229
ilov1985 <109591577+ilov1985@users.noreply.github.com>
2022-08-16 22:38:21 +0300

    Ex1

---
+++ Sem4_Ex1.cs
@@ -0,0 +1,24 @@
+﻿//Напишите цикл, который принимает
+//на вход два числа (А и В)
+using System;
+
+static class Program
+{
+	static void Main()
+	{
+		Console.Write("Введите число, которое будем возводить в натуральную степень: ");
+	    int a = Convert.ToInt32(Console.ReadLine());
+		
+        Console.Write("Введите степень: ");
+     	int b = Convert.ToInt32(Console.ReadLine());
+		
+    	double pow = a;
+		
+    	for (int i = 1;i < b; i++)
+    	{
+    		pow = pow * a;
+    	}
+		
+    	Console.WriteLine("Результат " + pow);
+	}
+}
