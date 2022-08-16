300ad2d18d92836b3dd13a0bc5188393a5593451
ilov1985 <109591577+ilov1985@users.noreply.github.com>
2022-08-16 23:32:34 +0300

    Ex3

---
+++ Sem4_Ex3.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу, которая
+//задает массив из N элементов
+//и выводит их на экран
+
+using System;
+
+static class Program
+{
+	static void Main()
+	{
+		Console.Write("Введите количество элементов массива: ");
+		int N = Convert.ToInt32(Console.ReadLine());
+		int [] array = new int[N];
+		for (int i = 0; i < N; i++)
+		{
+			array[i] = new Random().Next(1, N*10);
+			Console.Write(array[i] + " ");
+		}
+	}
+}
